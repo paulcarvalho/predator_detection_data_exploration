e53e36e4da750fe2bcd125bba908a513e26297cb
Paul Carvalho <pcarvalh@ucsc.edu>
2023-02-14 15:54:51 -0800

    load data from access db

--- pred_data_exploration.R
+++ pred_data_exploration.R
@@ -4,79 +4,45 @@
 # Author: Paul Carvalho
 #-------------------------------------------------------------------------------------------
 
+# Set up workspace ----
 # Clear workspace
 rm(list = ls())
 
 # Libraries
-library(RODBC)
+library(DBI)
+library(odbc)
+library(dbplyr)
 library(tidyverse)
 
 # Set working directory
-setwd("C:/Users/pgcar/Google Drive/1 Work/1 NOAA UCSC AT/1 Projects/Predator detections")
+setwd("~/github/predator_detection_data_exploration")
 
+# Pull data from access into R ----
 # Set up driver info and database path
-DRIVERINFO <- "Driver={Microsoft Access Driver (*.mdb, *.accdb)};"
-MDBPATH <- "C:/Users/pgcar/Google Drive/1 Work/1 NOAA UCSC AT/1 Projects/Predator detections/2014-2016_Predator_Detections.accdb"
-PATH <- paste0(DRIVERINFO, "DBQ=", MDBPATH)
+driverinfo <- "Driver={Microsoft Access Driver (*.mdb, *.accdb)};"
+mdbpath    <- "C:/Users/pgcar/Documents/github/predator_detection_data_exploration/2014-2016_Predator_Detections.accdb"
+path       <- paste0(driverinfo, "DBQ=", mdbpath)
 
 # Establish connection
-con <- odbcDriverConnect(PATH)
+con <- dbConnect(odbc(), .connection_string = path)
 
-# Load data into R dataframe
-predators <- sqlQuery(con,
-                      "SELECT *
-                      FROM Acoustic_tagged_Predators",
-                      stringsAsFactors = FALSE)
+# load data into R
+tagged_predators <- tbl(con, "Acoustic_tagged_Predators") %>%
+                    collect()
 
-detections <- sqlQuery(con,
-                       "SELECT *
-                       FROM predator_detections_final",
-                       stringsAsFactors = FALSE)
+deployments <- tbl(con, "Deployments") %>%
+               collect()
 
-deployments <- sqlQuery(con,
-                        "SELECT *
-                        FROM Deployments",
-                        stringsAsFactors = FALSE)
+monitor_locs <- tbl(con, "Monitor_locations") %>%
+                collect()
 
-locations <- sqlQuery(con,
-                      "SELECT *
-                      FROM Monitor_Locations",
-                      stringsAsFactors = FALSE)
+detections <- tbl(con, "predator_detections_final") %>%
+              select(datetime_PST, Receiver, Transmitter, `Station Name`, Receiver_short) %>%
+              collect()
 
-#-------------------------------------------------------------------------------------------
-# PREDATORS TABLE
-predators %>%
-   filter(Species_code == "STB" | Species_code == "LMB") %>%
-   select(Dates, Capture_site, Species_code, Length_mm, Weight_kg) %>%
-   group_by(Species_code, Capture_site) %>%
-   summarise(n = n()) %>%
-   mutate(Species_code = as.factor(Species_code),
-          Capture_site = factor(Capture_site, levels = c("A3","R3","C3","A2","R2","C2","A1","R1","C1"))) %>%
-   ggplot(data = .) +
-      geom_bar(aes(x = Capture_site, y = n), stat = 'identity') +
-      facet_grid(Species_code ~ .)
-
-#-------------------------------------------------------------------------------------------
-# DETECTIONS
-for(i in 1:length(detections)){
-   if(length(which(predators$TagID1 == detections$Transmitter[i])) != 0){
-      detections$TagSN[i] = predators$TagSN[which(predators$TagID1 == detections$Transmitter[i])]
-   } else if(length(which(predators$TagID2 == detections$Transmitter[i])) != 0){
-      detections$TagSN[i] = predators$TagSN[which(predators$TagID2 == detections$Transmitter[i])]
-   } else if(length(which(predators$TagID3 == detections$Transmitter[i])) != 0){
-      detections$TagSN[i] = predators$TagSN[which(predators$TagID3 == detections$Transmitter[i])]
-   }
-}
+# Merge data with detections ----
+df <- detections %>%
+   left_join(., deployments, by = c("Receiver_short" = "VR2SN")) %>%
+   left_join(., monitor_locs, by = "Location") %>%
+   filter((datetime_PST >= Start) & (datetime_PST <= Stop))
    
-
-detections %>%
-   mutate(TagSN = findTagSN(Transmitter))
-            
-   
-
-
-
-
-
-
-
